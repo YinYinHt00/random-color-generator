fdc9d94dfd4c480bc2a8d6fa1d7201ed2722987e
YinYinHt00 <yinyinhtoo25@gmail.com>
2023-08-10 14:28:30 +0630

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
+const mainPage = document.getElementById("container1");
+const simpleBackground = document.getElementById("simpleBg");
+const gradientBackground = document.getElementById("gradientBg");
+const Btn1 = document.getElementById("btn1");
+const Btn2 = document.getElementById("btn2");
 const bgColor = document.getElementById("bgColor");
-const colorButton = document.getElementById("colorButton");
-const colorText = document.getElementById("colorText");
-// const colors = ["white", "blue", "green", "yellow", "pink"];
+const simpleColorText = document.getElementById("simpleText");
+const simpleColorBtn = document.getElementById("simpleButton");
+const gradientColorText = document.getElementById("gradientText");
+const gradientColorBtn = document.getElementById("gradientButton");
 
-colorButton.addEventListener("click", () => {
-  // bgColor.style.backgroundColor = colors[Math.floor(Math.random() * 5)];
+Btn1.addEventListener("click", () => {
+  mainPage.style.display = "none";
+  simpleBackground.style.display = "block";
+});
+
+Btn2.addEventListener("click", () => {
+  mainPage.style.display = "none";
+  gradientBackground.style.display = "block";
+});
+
+simpleColorBtn.addEventListener("click", () => {
   const r = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
 
-  bgColor.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  colorText.innerHTML = `Current Color - RGB(${r}, ${g}, ${b})`;
-  if (r === 0 && g === 0 && b === 0) {
-    colorText.style.color = `rgb(255, 255, 255)`;
+  if (r < 100 && g < 100 && b < 100) {
+    simpleColorText.style.color = "#D8D9DA";
+    simpleColorBtn.style.backgroundColor = "#272829";
+    simpleColorBtn.style.color = "#D8D9DA";
+  } else {
+    simpleColorText.style.color = "#272829";
+    simpleColorBtn.style.backgroundColor = "#D8D9DA";
+    simpleColorBtn.style.color = "#272829";
   }
 
-  // bgColor.style.backgroundColor = `linear-gradient( ${(r, g, b)}, ${(r, g, b)})`;
+  bgColor.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  simpleColorText.innerHTML = `Current Color - RGB(${r}, ${g}, ${b})`;
+});
+
+gradientColorBtn.addEventListener("click", () => {
+  const r1 = Math.floor(Math.random() * 256);
+  const b1 = Math.floor(Math.random() * 256);
+  const g1 = Math.floor(Math.random() * 256);
+  const r2 = Math.floor(Math.random() * 256);
+  const b2 = Math.floor(Math.random() * 256);
+  const g2 = Math.floor(Math.random() * 256);
+  // const direction = Math.floor(Math.random() * 361);
+  // colorText.innerHTML = `Current Gradient Color - linear-gradient(${direction}deg, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
+  // bgColor.style.background = `linear-gradient(${direction}deg, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
+  bgColor.style.background = `linear-gradient(40deg, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
+  gradientColorText.innerHTML = `Current Color -
+  linear-gradient(rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
 });
